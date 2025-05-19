527de0b7a7d0196ba98538c7f408ef11027c74c7
Alok963-ai <alokjangde6@gmail.com>
2025-05-20 03:23:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ API_HASH  = os.environ.get("API_HASH", "e8cb5fb7a475b5f5eb3b0ef0e6ca03a8")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 AUTH_USERS = [7833842279]
 #WEBHOOK = True  # Don't change this
-#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+#PORT = int(os.environ.get("PORT", 8000))  # Default to 8000 if not set
