d84f9edddfcbee37802124e3baf5fdcb9cb923ff
Alok963-ai <alokjangde6@gmail.com>
2025-05-20 02:56:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "20288951"
-api_hash = "e8cb5fb7a475b5f5eb3b0ef0e6ca03a8"
-bot_token = "enter your bot token"
-auth_users = [7833842279]
+API_ID = "20288951"
+API_HASH = "e8cb5fb7a475b5f5eb3b0ef0e6ca03a8"
+BOT_TOKEN = "enter your bot token"
+AUTH_USERS = [7833842279]
