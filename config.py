fa559aebc8d18527c35557467821c28b7b2ca984
Alok963-ai <alokjangde6@gmail.com>
2025-05-20 03:17:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
-API_ID = "20288951"
-API_HASH = "e8cb5fb7a475b5f5eb3b0ef0e6ca03a8"
-BOT_TOKEN = "enter your bot token"
+import os
+
+API_ID    = os.environ.get("API_ID", "20288951")
+API_HASH  = os.environ.get("API_HASH", "e8cb5fb7a475b5f5eb3b0ef0e6ca03a8")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 AUTH_USERS = [7833842279]
+#WEBHOOK = True  # Don't change this
+#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
